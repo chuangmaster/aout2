df7a8e3a45a652fb7c06cfb0a62f030916122d78
chuangmaster <chuang.master@gmail.com>
2024-04-02 02:33:26 +0800

    加入專案檔案。

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a131a1e5-c6e3-49c5-8435-392c23aa517c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using LogAn;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        //[Test]
+        //public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        //{
+
+        //    LogAnalyzer analyzer = new LogAnalyzer();
+
+        //    bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+
+        //    Assert.That(result, Is.False, "這是被log的文字");
+
+        //    //NUnit 4.0以前寫法
+        //    //ClassicAssert.True(result);
+        //}
+
+        //[Test]
+        //public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
+        //{
+        //    LogAnalyzer analyzer = new LogAnalyzer();
+
+        //    bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+
+        //    Assert.That(result, Is.True);
+        //}
+
+        //[Test]
+        //public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
+        //{
+        //    LogAnalyzer analyzer = new LogAnalyzer();
+
+        //    bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+
+        //    Assert.That(result, Is.True);
+        //}
+
+
+        #region Use Test Case
+        [TestCase("filewithgoodextension.slf")]
+        [TestCase("filewithgoodextension.SLF")]
+
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue(string fileName)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName(fileName);
+
+            Assert.That(result, Is.True);
+        }
+        [TestCase("filewithgoodextension.slf", true)]
+        [TestCase("filewithgoodextension.SLF", true)]
+        [TestCase("filewithgoodextension.foo", false)]
+        [TestCase("filewithgoodextension.Foo", false)]
+        public void IsValidLogFileName_VariousExtensions_CheksThem(string fileName, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(fileName);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        #endregion
+
+
+    }
+}
