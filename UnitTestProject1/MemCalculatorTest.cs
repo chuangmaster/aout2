67bb8ad8382a0960a237d9924965efc1e4635d75
chuangmaster <chuang.master@gmail.com>
2024-04-05 11:38:16 +0800

    Add MemCalculator and its test

---
+++ UnitTestProject1/MemCalculatorTest.cs
@@ -0,0 +1,35 @@
+﻿using LogAn;
+using NUnit.Framework;
+using System.Management.Instrumentation;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestFixture]
+    public class MemCalculatorTest
+    {
+        [Test]
+        public void Sum_WhenCalled_ReturnsZero()
+        {
+            MemCalculator calculator = MakeCalculator();
+            int lastSum = calculator.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangeSum()
+        {
+            MemCalculator calculator = MakeCalculator();
+            calculator.Add(1);
+            int sum = calculator.Sum();
+            Assert.AreEqual(1, sum);
+        }
+
+        public MemCalculator MakeCalculator()
+        {
+            return new MemCalculator();
+        }
+    }
+}
