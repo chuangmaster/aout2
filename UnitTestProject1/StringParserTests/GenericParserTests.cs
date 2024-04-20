9ee9e7323796863209b3540465f84a6d23d6cb9a
chuangmaster <chuang.master@gmail.com>
2024-04-20 19:43:58 +0800

    Add Generic version

---
+++ UnitTestProject1/StringParserTests/GenericParserTests.cs
@@ -0,0 +1,26 @@
+﻿using aout2.Parser;
+using NUnit.Framework;
+using System;
+
+
+namespace UnitTestProject1.StringParserTests
+{
+    public abstract class GenericParserTests<T> where T : IStringParser
+    {
+        protected abstract string GetInputHeaderSingleDigit();
+
+        protected T GetParser(string input)
+        {
+            return (T)Activator.CreateInstance(typeof(T), input);
+        }
+
+        [Test]
+        public void TestGetTextVersionFromHeader_SingleDigit_Found()
+        {
+            string input = GetInputHeaderSingleDigit();
+            T parser = GetParser(input);
+            var result = parser.GetTextVersionFromHeader();
+            Assert.AreEqual("1", result);
+        }
+    }
+}
---
+++ UnitTestProject1/StringParserTests/StandardStringParserTestsByGeneric.cs
@@ -0,0 +1,20 @@
+﻿using aout2.Parser;
+using NUnit.Framework;
+
+namespace UnitTestProject1.StringParserTests
+{
+    [TestFixture]
+    public class StandardStringParserTestsByGeneric : GenericParserTests<StandardStringParser>
+    {
+        //protected override string HeaderVersion_SingleDigit => $"header;version={EXPECTED_SINGLEDIGIT};";
+
+        //protected override string HeaderVersion_WithMinorVersionDigit => $"header;version={EXPECTED_WITHMINORVERSIONDIGIT};";
+
+        //protected override string HeaderVersion_WithRevision => $"header;version={EXPECTED_WITHREVISION};";
+
+        protected override string GetInputHeaderSingleDigit()
+        {
+            return $"header;version=1";
+        }
+    }
+}
